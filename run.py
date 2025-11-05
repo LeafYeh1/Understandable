4ca11071f220a23a0e197f90368b27159c7e9b10
Leaf <beepee7749@gmail.com>
2025-11-05 15:02:14 +0800

    Fix: heroku.yml app to run

--- run.py
+++ run.py
@@ -8,5 +8,3 @@ if __name__ == "__main__":
     with app.app_context():
         # 確保資料庫被建立
         db.create_all() 
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
